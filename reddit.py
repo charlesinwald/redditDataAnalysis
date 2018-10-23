8e205b96fd0b8d91e12d8ed50a438e6caee7ae48
Charles Inwald <cci219@lehigh.edu>
2018-10-23 11:41:02 -0400

    Better analysis, prettier strings

--- reddit.py
+++ reddit.py
@@ -1,26 +1,41 @@
+#Reddit scraper
 import praw
+#Natural Language Processing
+import spacy
+from collections import Counter
+
+nlp = spacy.load('en_core_web_sm')
+
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learnpython")
+subreddit = reddit.subreddit("Jokes")
 data = []
-for submission in subreddit.hot(limit=50):
+#Gather Reddit posts
+for submission in subreddit.hot(limit=200):
     tuple = (submission.title,submission.selftext,submission.score)
     data.append(tuple)
-#print("---------------------------------\n")
-#print(data)
 i = 0
 cani = []
-#for row in data:
+correct = []
+#For each post, perform NLP
 for num, row in enumerate(data, start=0):
-	#print(row[0] + '\n')
-#	print("---------------------------------\n")
-#	print(row[1] + '\n')
-#	print("---------------------------------\n")
 	message = row[1]
-	if ("Can I" in message) or ("the" in row[0]):
-#		print('FOUND ----------------------\n\n\n')
-		cani.append(row)
-	#print(num)
-for row in cani:
-	print(row[0])
-	print('\n')
+	cani.append(row)
+	print(row[i])
+	doc = nlp(row[i])
+	for entity in doc.ents:
+		print(entity.text, entity.label_,'\n')
+		correct.append(entity.text)
+counts = Counter(correct)
+counts2 = []
+for element in correct:
+	print(element, counts[element],  '\n')
+	counts2.append((counts[element],element))
+counts2 = set(sorted(counts2, reverse=True))
+counts3 = sorted(counts2, reverse=True)
+for element in counts3:
+	print(element)
+	for i in range(0, element[0]):
+		print('[-]', end='')
+	print('\n---------------------------------------------------');
+print('---------------------------------------------------');
